7263a3a5c5e0afcc2349bb81ef0007c65be04cff
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-10-20 03:37:42 +0900

    update tmux-setup.sh

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -10,12 +10,12 @@ tmux split-window -v -t "$SESSION" # for client start
 tmux split-window -v -t "$SESSION" # for server generate
 tmux split-window -v -t "$SESSION" # for server start
 
-tmux send-keys -t "$SESSION":0.0 '(cd client && npm run generate)' C-m
+tmux send-keys -t "$SESSION":0.0 '(cd client && npm run codegen)' C-m
 tmux send-keys -t "$SESSION":0.1 '(cd client && npm start)' C-m
-tmux send-keys -t "$SESSION":0.2 '(cd server && npm run generate)' C-m
+tmux send-keys -t "$SESSION":0.2 '(cd server && npm run codegen)' C-m
 tmux send-keys -t "$SESSION":0.3 '(cd server && npm start)' C-m
 
-# vertical 
+# Set the even-vertical layout 
 tmux send-keys -t "$SESSION" M-2
 
 tmux attach -t "$SESSION"
